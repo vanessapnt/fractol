ed25e4a738d6ce9a965ac63ac52b64675ce5c80d
Vanessa Rodrigues <varodrig@made-f0Br8s5.clusters.42paris.fr>
2024-10-15 19:42:27 +0200

    position

--- draw.c
+++ draw.c
@@ -6,16 +6,50 @@
 /*   By: varodrig <varodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 18:54:44 by varodrig          #+#    #+#             */
-/*   Updated: 2024/10/14 18:58:17 by varodrig         ###   ########.fr       */
+/*   Updated: 2024/10/15 19:39:20 by varodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
+// 0 - WIDTH -> -2 - 2
+// 0 - HEIGTH -> 2 - -2
+
+mandelbrot_julia(t_complex z, t_complex *c, t_fractal *fractal)
+{
+	if (!strncmp(fractal->name, "julia", 5))
+	{
+		c->x = julia_x;
+		c->y = julia_y;
+	}
+	else
+	{
+		c->x = z.x;
+		c->y = z.y;
+		z.x = 0;
+		z.y = 0;
+	}
+}
+
+ft_pixels(int x, int y, t_fractal *fractal)
+{
+	t_complex	z;
+	t_complex	c;
+	int			i;
+
+	z.x = (map(x, -2, 2, 0, WIDTH) * fractal->zoom) + fractal->shift_x;
+	z.y = (map(y, 2, -2, 0, HEIGHT) * fractal->zoom) + fractal->shift_y;
+	mandelbrot_julia(z, &c, fractal);
+	while (i < fractal->iterations)
+	{
+		z = ()
+	}
+}
+// int x, y bc it's size
 fractal_draw(t_fractal *fractal)
 {
-	int	x;
-	int	y;
+	int x;
+	int y;
 
 	y = 0;
 	while (y <= HEIGHT)
@@ -23,7 +57,7 @@ fractal_draw(t_fractal *fractal)
 		x = 0;
 		while (x <= WIDTH)
 		{
-			
+			ft_pixels(x, y, fractal);
 		}
 	}
 }
--- fractol.h
+++ fractol.h
@@ -1,14 +1,21 @@
 #ifndef FRACTOL_H
-#define FRACTOL_H
+# define FRACTOL_H
 
-#include "mlx/mlx.h"
+# include "mlx/mlx.h"
 
 typedef struct s_fractal
 {
-	char *name;
+	char	*name;
 	void	*mlx_ptr;
 	void	*mlx_win_ptr;
-}	t_fractal
+}			t_fractal;
 
+typedef struct s_complex
+{
+	double	x;
+	double	y;
+}			t_complex;
+
+fractal_draw(t_fractal *fractal);
 
 #endif
